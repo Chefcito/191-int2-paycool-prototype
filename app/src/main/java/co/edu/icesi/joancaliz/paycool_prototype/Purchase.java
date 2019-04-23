6095a9f64d9af1bde6bd741a0179d091670c4336
AnotherPotato <joansebastian35@gmail.com>
2019-04-23 09:35:01 -0500

    Purchase class added

---
+++ app/src/main/java/co/edu/icesi/joancaliz/paycool_prototype/Purchase.java
@@ -0,0 +1,78 @@
+package co.edu.icesi.joancaliz.paycool_prototype;
+
+public class Purchase {
+    private int price1, price2, price3, discount, subtotal, total;
+    private String code;
+
+    public Purchase() {
+
+    }
+
+    public Purchase(int discount, String code) {
+        price1 = (int) Math.random()*10000;
+        price2 = (int) Math.random()*10000;
+        price3 = (int) Math.random()*10000;
+        this.discount = discount;
+
+        subtotal = price1 + price2 + price3;
+        total = subtotal - discount;
+
+        this.code = code;
+    }
+
+    public int getPrice1() {
+        return price1;
+    }
+
+    public void setPrice1(int price1) {
+        this.price1 = price1;
+    }
+
+    public int getPrice2() {
+        return price2;
+    }
+
+    public void setPrice2(int price2) {
+        this.price2 = price2;
+    }
+
+    public int getPrice3() {
+        return price3;
+    }
+
+    public void setPrice3(int price3) {
+        this.price3 = price3;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(int subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
