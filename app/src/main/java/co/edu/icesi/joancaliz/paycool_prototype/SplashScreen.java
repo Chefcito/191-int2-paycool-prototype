8f7b5d894f2c34df4121ad3cb0486eb8486ef89e
Christopher <Chris.97e@gmail.com>
2019-04-22 22:54:29 -0500

    Se crea la splash Screen

---
+++ app/src/main/java/co/edu/icesi/joancaliz/paycool_prototype/SplashScreen.java
@@ -0,0 +1,35 @@
+package co.edu.icesi.joancaliz.paycool_prototype;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //Hacer Full Screen la pantalla
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        //Crear el dealy del splash screen a través de Handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+            }
+        },3000);
+
+
+    }
+}
