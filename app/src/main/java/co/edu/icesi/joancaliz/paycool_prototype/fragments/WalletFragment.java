5ea45521cc6b25b4ab99a02587095ab5db8b0b4c
AnotherPotato <joansebastian35@gmail.com>
2019-05-19 16:14:59 -0500

    Wallet fragment updated

--- app/src/main/java/co/edu/icesi/joancaliz/paycool_prototype/fragments/WalletFragment.java
+++ app/src/main/java/co/edu/icesi/joancaliz/paycool_prototype/fragments/WalletFragment.java
@@ -9,13 +9,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import co.edu.icesi.joancaliz.paycool_prototype.R;
+import co.edu.icesi.joancaliz.paycool_prototype.User;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 public class WalletFragment extends Fragment {
+
+    private View view;
+
+    private TextView money;
+
+    private FirebaseAuth auth;
+    private DatabaseReference dbReference;
+    private DatabaseReference dbUsersReference;
 
     private static final int TRANSFER_REQUEST = 1;
 
@@ -25,13 +42,33 @@ public class WalletFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+        view = inflater.inflate(R.layout.fragment_wallet, container, false);
+
+        money = view.findViewById(R.id.fragment_wallet_money_text_view);
 
         withdrawButton = view.findViewById(R.id.fragment_wallet_withdraw_button);
         withdrawButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToTransferActivity();
+            }
+        });
+
+        //Firebase
+        auth = FirebaseAuth.getInstance();
+        dbReference = FirebaseDatabase.getInstance().getReference();
+        dbUsersReference = dbReference.child("Users");
+
+        dbUsersReference.child(auth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user  =  dataSnapshot.getValue(User.class);
+                money.setText("$" + Integer.toString(user.getMoney()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("Error: " + databaseError);
             }
         });
 
